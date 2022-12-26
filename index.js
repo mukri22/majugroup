e81b800044ce21e82637f1f334573c98457181f4
Sulthon Yoga <sulthonyoga22@gmail.com>
2022-12-26 19:56:23 +0700

    first commit

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
 import AdminRoute from "./routes/AdminRoute.js";
-// import multer from "multer";
-// import FileUpload from "express-fileupload";
-// import cloudinary from "./utils/cloudinary.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
@@ -18,61 +18,48 @@ const app = express();
 const sessionStore = SequelizeStore(session.Store);
 
 const store = new sessionStore({
-    db: db
+  db: db,
 });
-// const fileStorage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'images');
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, new Date().toString() + '-' + file.originalname)
-//     }
-// })
-
-// const fileFilter = (req, file, cb) => {
-//     if(
-//         file.mimetype === 'image/png' ||
-//         file.mimetype === 'image/jpg' ||
-//         file.mimetype === 'image/jpeg'
-//     ){
-//         cb(null, true);
-//     }else{
-//         cb(null, false);
-//     }
-// }
-
 
 // (async()=>{
 //     await db.sync();
 // })();
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
     store: store,
     cookie: {
-        secure: 'auto'
-    }
-}));
+      secure: "auto",
+    },
+  })
+);
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: 'http://localhost:3000'
-}));
-app.use(express.json({limit: '50mb'}));
-// app.use(express.urlencoded({ limit: '50mb', extended: true }));
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json({ limit: "50mb" }));
 app.use(UserRoute);
 app.use(ProductRoute);
 app.use(AdminRoute);
 app.use(AuthRoute);
-// app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
-// app.use(FileUpload());
-// app.use(express.static("public"));
-// app.use(cloudinary);
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    limit: "100mb",
+    extended: true,
+  })
+);
+app.use(cookieParser());
 
 // store.sync();
 
-app.listen(process.env.APP_PORT, ()=> {
-    console.log('Server up and running...');
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server up and running...");
 });
--- models/ProductModel.js
+++ models/ProductModel.js
@@ -2,16 +2,18 @@ import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 import Users from "./UserModel.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
-const Products = db.define('product',{
-    uuid:{
-        type: DataTypes.STRING,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        validate:{
-            notEmpty: true
-        }
+const Products = db.define(
+  "product",
+  {
+    uuid: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     // name:{
     //     type: DataTypes.STRING,
@@ -28,50 +30,47 @@ const Products = db.define('product',{
     //         notEmpty: true
     //     }
     // },
-    kematian:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate:{
-            notEmpty: false
-        }
+    kematian: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
     },
-    bobot:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate:{
-            notEmpty: false
-        }
+    bobot: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
     },
-    pakan:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate:{
-            notEmpty: false
-        }
+    pakan: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: false,
+      },
     },
-    userId:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate:{
-            notEmpty: true
-        }
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     image: {
-        type: DataTypes.STRING,
-        // value:('public_id', 'url'),
-    }
-    // url:{
-    //     type: DataTypes.STRING
-
-    // }
-},{
-    freezeTableName: true
-});
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 Users.hasMany(Products);
-Products.belongsTo(Users, {foreignKey: 'userId'});
+Products.belongsTo(Users, { foreignKey: "userId" });
 
 export default Products;
-// (async()=>{
-//     await db.sync();
-//     })();
